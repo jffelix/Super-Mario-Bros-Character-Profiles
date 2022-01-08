62556b92a37c253836573f7968f816c97f9ae561
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-01-08 14:51:40 -0800

    Added Hammer Bro entry w/ CSS styling

--- client/src/characterProfiles.js
+++ client/src/characterProfiles.js
@@ -10,14 +10,14 @@ var characterProfiles = [
         fullName: "Luigi Mario",
         species: "human",
         url: "https://thumbs.gfycat.com/WhirlwindPleasedDipper-size_restricted.gif",
-        description: "Deuteragonist of the Super Mario Series and Mario Mario's brother.",
+        description: "The younger fraternal twin brother and sidekick of Mario.",
         stylingClass: "luigi"
     },
     {
         fullName: "Goomba",
         species: "goomba",
         url: "https://images.gamebanana.com/img/ico/sprays/goomba_2.gif",
-        description: "The Goombas once lived in peace in the Mushroom Kingdom, but they later betrayed them and joined the Koopa Troop. The Goombas are also probably the most common enemy in the entire Mario franchise.",
+        description: "They once lived in peace in the Mushroom Kingdom, but they later betrayed them and joined the Koopa Troop. They are the most common enemy in the entire Mario franchise.",
         stylingClass: "goomba"
     },
     {
@@ -26,6 +26,13 @@ var characterProfiles = [
         url: "https://art.pixilart.com/a4e9a3b99b7bd84.gif",
         description: "A race of humanoid turtles that serve under their King, Bowser.",
         stylingClass: "koopa"
+    },
+    {
+        fullName: "Hammer Bro",
+        species: "koopa",
+        url: "https://www.andersonkenya1.net/uploads/monthly_2018_08/hammerbro.thumb.gif.ae1fc7b62158f4147d6a67c1e2429906.gif",
+        description: "They are specialized Koopa Troopas commonly found in pairs (though not exclusively so) who wear light armor and utilize hammers as their main weapons.",
+        stylingClass: "hammerBro"
     }
     // {
     //     fullName: "King Bowser Koopa",
