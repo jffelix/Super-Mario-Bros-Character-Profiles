a6532386493126581309d49e443ae1ed5dba44b3
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-01-08 12:22:42 -0800

    Implemented profile toggle feature

--- client/src/components/sub-components/CharactersListEntry.jsx
+++ client/src/components/sub-components/CharactersListEntry.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CharacterListEntry = (props) => {
 
     // when character is clicked
       // description will render
 
+    const [ wasProfileClicked, setWasProfileClicked] = useState(false);
+
+    const expandProfile = () => {
+        setWasProfileClicked(!wasProfileClicked);
+    }
+
     return (
         <div>
             <div>
-                <img className={props.entry.stylingClass} src={props.entry.url} />
+                <img 
+                    className={props.entry.stylingClass} 
+                    src={props.entry.url}
+                    onClick={expandProfile} />
             </div>
-            <div className="profile">
-                <div className="fullName">
-                    <p>{props.entry.fullName}</p>
-                </div>
-                <div className="species">
-                    <p>{props.entry.species}</p>
-                </div>
-                <div className="description">
-                    <p>{props.entry.description}</p>
-                </div>
-            </div>
+            { wasProfileClicked ? 
+            
+                <div className="profile">
+                    <div className="fullName">
+                        <p>{props.entry.fullName}</p>
+                    </div>
+                    <div className="species">
+                        <p>{props.entry.species}</p>
+                    </div>
+                    <div className="description">
+                        <p>{props.entry.description}</p>
+                    </div>
+                </div> : null
+            }
         </div>
     )
 
