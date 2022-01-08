9f027fc624cdd0a43bc9b7d529cd6983c3c1a08b
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-01-08 02:26:31 -0800

    Added CSS background image

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CharacterList from "./sub-components/CharactersList.jsx";
+import "../../dist/styles.css";
 
 const App = () => {
 
